80703c8a1fc4c493c3e03c44c2c22f8d9842dc5d
b7008109@my.shu.ac.uk <bentyzack@hotmail.co.uk>
2022-02-02 18:24:50 +0000

    Created login system for frontend and connect to server

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,73 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">Bookstore</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/users" class="nav-link">List Users</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add-users" class="nav-link">Add Users</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/public" class="nav-link">
+            <font-awesome-icon icon="home" />Public
+          </router-link>
+        </li>
+        <li v-if="protectedContent" class="nav-item">
+          <router-link to="/protectedcontent" class="nav-link">Protected</router-link>
+        </li>
+      </div>
+      <div v-if="!currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">
+            <font-awesome-icon icon="user-plus" />Sign Up
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">
+            <font-awesome-icon icon="sign-in-alt" />Login
+          </router-link>
+        </li>
+      </div>
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+            <font-awesome-icon icon="user" />
+            {{ currentUser.username }}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href @click.prevent="logOut">
+            <font-awesome-icon icon="sign-out-alt" />LogOut
+          </a>
+        </li>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+    protectedContent() {
+      return (this.currentUser?true:false);
+    }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
   }
-}
+};
 </script>
 
 <style>
