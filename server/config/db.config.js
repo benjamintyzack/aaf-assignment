7eb0aac2e4f914ee3420be408db0547ebbb39b6b
b7008109@my.shu.ac.uk <bentyzack@hotmail.co.uk>
2022-02-09 17:21:07 +0000

    updated backend to use localhost DB

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://admin:admin@bookshopcluster.xqmhf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    url: "mongodb://localhost:27017/bookstore"
 };
