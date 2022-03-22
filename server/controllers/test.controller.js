d61a066783c379ab7ec0c579a8617c18a1fb4a21
b7008109@my.shu.ac.uk <bentyzack@hotmail.co.uk>
2022-03-22 14:24:23 +0000

    removed irrelevant file

--- server/controllers/test.controller.js
+++
@@ -1,7 +0,0 @@
-exports.publicContent = (req, res) => {
-    res.status(200).send("Public Content.");
-  };
-  
-  exports.protectedContent = (req, res) => {
-    res.status(200).send("User Protected Content.");
-  };  
